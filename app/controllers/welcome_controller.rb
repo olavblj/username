d0a8515999a9c0ae8d6513905c85b5ae5b9e8567
Olav Ljosland <olavlj@me.com>
2015-01-13 16:01:21 +0100

    Får nytt brukernavn uten å refreshe

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-  	@words = Word.all
-  	@nouns = Word.where(klasse: "noun")
-  	@verbs = Word.where(klasse: "verb")
+  	@noun = Word.where(klasse: "noun").sample
+  	@verb = Word.where(klasse: "verb").sample
+
+  	respond_to do |format|
+  		format.html
+  		format.json {render json: { verb: @verb.ord, noun: @noun.ord}}
+
+  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :words
 
   root 'welcome#index'
-
+  get '/index', to: 'welcome#index'
 end
